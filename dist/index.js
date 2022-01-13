655789cac14209c661dcb604171fc21c75c477ce
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2022-01-13 17:55:17 +0530

    Update compiled code

--- dist/index.js
+++ dist/index.js
@@ -25405,7 +25405,7 @@ async function run() {
     const targetBranchesArray = targetBranches.split(",");
     const repo = { repo: githubRepo.split("/")[1], owner: githubRepo.split("/")[0] };
 
-    const octokit = new github.GitHub(githubToken);
+    const octokit = new github.GitHub(null, {auth: githubToken});
 
     const { data: currentPulls } = await octokit.pulls.list(repo);
 
